c1ec8b1024733fca69e75b6ef7e6e5f625a64a8f
BenShieh233 <bensshieh4333@gmail.com>
2023-10-14 03:02:35 -0500

    allow webscraping.py to grasp both 10-Q and 10-K files

--- webscraping.py
+++ webscraping.py
@@ -7,29 +7,31 @@ class SECScraper:
         self.FILE = FILE
 
     def scrape_sec_data(self):
-        quarters = ['QTR1', 'QTR2', 'QTR3', 'QTR4']
-        for quarter in quarters:
-            url = f'https://www.sec.gov/Archives/edgar/full-index/{self.Year}/{quarter}/master.idx'
-            headers = {'User-Agent': 'bxie43@wisc.edu'}
-            req = urllib.request.Request(url, headers=headers)
-
-            try:
+        urls = []
+        try:            
+            quarters = ['QTR1', 'QTR2', 'QTR3', 'QTR4']
+            string_match1 = 'edgar/data/'
+            for quarter in quarters:
+                url = f'https://www.sec.gov/Archives/edgar/full-index/{self.Year}/{quarter}/master.idx' # Search for master index for each quarter
+                headers = {'User-Agent': 'bxie43@wisc.edu'}
+                req = urllib.request.Request(url, headers=headers)
                 response = urllib.request.urlopen(req)
                 element2, element3, element4 = None, None, None
-                string_match1 = 'edgar/data/'
-
                 for line in response:
                     content = line.decode('utf-8')
-                    if self.CIK in content and self.FILE in content:
+                    if self.CIK in content and self.FILE in content: # Check if CIK and FILE are within the content
                         if string_match1 in content:
-                            element2 = content.split('|')
+                            element2 = content.split('|') 
                             for element3 in element2:
-                                if string_match1 in element3:
-                                    element4 = element3.strip()
-                if element4:
-                    url3 = 'https://www.sec.gov/Archives/' + element4
-                    return url3
-                
-            except urllib.error.URLError as e:
-                return f"Failed to retrieve content. Error: {e}" 
+                                if string_match1 in element3: # Extract the headers for the corresponding document
+                                    element4 = element3.strip() # Remove the whitespace
+                                    if element4 and f'edgar/data/{self.CIK}' in element4: # Check if the url has the CIK right after the string_match1
+                                         url3 = 'https://www.sec.gov/Archives/' + element4 # Return the complete url
+                                         
+                                         urls.append(url3)
+            return urls
+
+            
+        except urllib.error.URLError as e:
+            return f"Failed to retrieve content. Error: {e}" 
         
