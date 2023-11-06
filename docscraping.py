f74077a32204bef395bb619c4275e3fcf3b1b5e1
BenShieh43 <125306044+BenShieh43@users.noreply.github.com>
2023-11-05 20:34:33 -0600

    Add files via upload

--- docscraping.py
+++ docscraping.py
@@ -1,19 +1,114 @@
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 import re
+import html
+from cikscraping import CIKScraper
+from webscraping import SECScraper
+from sec_api import ExtractorApi
+
+
+def text_preprocessing(text):
+    text = html.unescape(text)
+
+    # TABLE_START and ##TABLE_END
+    text = re.sub(r'##TABLE_START|##TABLE_END', '', text)
+
+    return text
+
+def regulatory_compliance(item1a):
+    # Preprocess Section Item 1A
+    text = text_preprocessing(item1a)
+
+    # Remove 
+    text = re.sub(r'\n\n', '', text)
+
+    # Define a regular expression pattern to capture risks and their descriptions
+    pattern = r'([A-Z][a-zA-Z\s]+ Risks)'
+
+    # Find all matches in the text
+    matches = re.findall(pattern, text)
+
+    # Create a dictionary and assign the text behind each risk
+    risks_dict = {}
+    text_segments = re.split(pattern, text)[1:]
+
+    current_risk = None
+
+    for item in text_segments:
+        if item in matches:
+            current_risk = item
+        elif current_risk is not None:
+            risks_dict[current_risk] = item
+            current_risk = None
+
+    regulatory_compliance = {key: value for key, value in risks_dict.items() if "legal" in key.lower() or "regulatory" in key.lower()}
+
+    return regulatory_compliance
+
+def product_portofolio(item1a):
+    pattern = r'\n\n([A-Za-z, ]+)\n\n'
+    matches = re.findall(pattern, item1a)
+
+    intro_dict = {}
+    text_segments = re.split(pattern, item1a)[1:]
+
+    # Initialize a dictionary to store the pairs
+    title_sentence_pairs = {}
+    current_title = None
+    current_sentences = []
+
+    # Iterate through the sentences and group them under the titles
+    for item in text_segments:
+        if item in matches:
+            if current_title:
+                title_sentence_pairs[current_title] = current_sentences
+            current_title = item
+            current_sentences = []
+        else:
+            current_sentences.append(item)
+
+    # Add the last set of sentences
+    if current_title:
+        title_sentence_pairs[current_title] = ' '.join(current_sentences)
+
+    return title_sentence_pairs
+
+def acquisitions(item8):
+    text = text_preprocessing(parser.item8)
+    pattern = r'\n\n(.*?acquisition.*?)\n\n'
+
+    #matches = re.findall(pattern, text, re.IGNORECASE)
+
+    #intro_dict = {}
+    text_segments = re.split(pattern, text)[1:]
+
+    return text_segments
+
 
 class DocScraper:
-    def __init__(self, url, FILE, headers):
+    def __init__(self, url, FILE, headers, api_key, year):
         self.url = url
         self.headers = headers
         self.file = FILE
+        self.api_key = api_key
+        self.year = year
+    
+        # Store the item sections within 10-K report
         self.document = None
+        self.item1 = None
+        self.item1a = None
+        self.item2 = None
+        self.item7 = None
+        self.item8 = None
+
+        # Store the text variables
+        self.regulatorycompliance = None
+        self.productportfolio = None
+        self.acquisitions = None
+
 
     def parsing_file(self):
         r = requests.get(self.url, headers = self.headers)
         raw_file = r.text
-
         doc_start_pattern = re.compile(r'<DOCUMENT>')
         doc_end_pattern = re.compile(r'</DOCUMENT>')
         type_pattern = re.compile(r'<TYPE>[^\n]+')
@@ -25,68 +120,46 @@ class DocScraper:
         for doc_type, doc_start, doc_end in zip(doc_types, doc_start_is, doc_end_is):
             if doc_type == self.file:
                 document[doc_type] = raw_file[doc_start:doc_end]
-        regex = re.compile(r'(>Item(\s|&#160;|&nbsp;)(1A|1B|7A|7|8|9A)\.{0,1})|(ITEM\s(1A|1B|7A|7|8|9A))')
-        matches = regex.finditer(document[self.file])
 
-        # for match in matches:
-        #     print(match)
+        # Store the complete document
+        self.document = document 
 
-        # Create the dataframe
-        test_df = pd.DataFrame([(x.group(), x.start(), x.end()) for x in matches])
-
-        test_df.columns = ['item', 'start', 'end']
-        test_df['item'] = test_df.item.str.lower()
-
-        # Get rid of unnesesary charcters from the dataframe
-        test_df.replace('&#160;',' ',regex=True,inplace=True)
-        test_df.replace('&nbsp;',' ',regex=True,inplace=True)
-        test_df.replace(' ','',regex=True,inplace=True)
-        test_df.replace('\.','',regex=True,inplace=True)
-        test_df.replace('>','',regex=True,inplace=True)
-
-        pos_dat = test_df.sort_values('start', ascending=True).drop_duplicates(subset=['item'], keep='last')
-        pos_dat.set_index('item', inplace=True)
-
-        self.document = document
-
-        return pos_dat
-
-
-
-    def get_fin(self):
-
-        pos_dat = self.parsing_file()
-
-        # Create a dictionary to store item content
-        item_content = {}
-
-        # Get Item 1a
-        item_1a_raw = self.document[self.file][pos_dat['start'].loc['item1a']:pos_dat['start'].loc['item1b']]
-        item_1a_content = BeautifulSoup(item_1a_raw, 'lxml')
-        item_content['regulatory_compliance'] = item_1a_content.get_text().replace('\xa0', ' ')
-
-        # Get Item 7
-        item_7_raw = self.document[self.file][pos_dat['start'].loc['item7']:pos_dat['start'].loc['item7a']]
-        item_7_content = BeautifulSoup(item_7_raw, 'lxml')
-        item_content['sales_performance'] = item_7_content.get_text().replace('\xa0', ' ')
-
-        # Get Item 7a
-        item_7a_raw = self.document[self.file][pos_dat['start'].loc['item7a']:pos_dat['start'].loc['item8']]
-        item_7a_content = BeautifulSoup(item_7a_raw, 'lxml')
-        item_content['market_risks'] = item_7a_content.get_text().replace('\xa0', ' ')
+        extractorApi = ExtractorApi(self.api_key)
         
-        return item_content
+        # Extract Item 1, 1A, 2, 7, and 8
+        self.item1 = extractorApi.get_section(self.url, "1", "text")
+        self.item1a = extractorApi.get_section(self.url, "1A", "text")
+        self.item2 = extractorApi.get_section(self.url, "2", "text")
+        self.item7 = extractorApi.get_section(self.url, "7", "text")
+        self.item8 = extractorApi.get_section(self.url, "8", "text")
+
+        self.regulatorycompliance = regulatory_compliance(self.item1a)
+        self.productportfolio = product_portofolio(self.item1)
+        self.acquisitions = acquisitions(self.item8)
+
+if __name__ == "__main__":
+    headers = {"User-Agent": "bxie43@wisc.edu"}
+    FILE = '10-K'
+    Year = '2023'
+    api_key = '053cfbf1aa19a8f110d11a302baa4b045cb7ac2448d6c117ffa134e6ca68afdc'
 
 
-        
+    company = CIKScraper("ADM", headers)
+    company.parsing_tickers()
 
+    scraper = SECScraper(company.cik, Year, FILE, headers)
+    url = scraper.scrape_sec_data()[0]
 
+    parser = DocScraper(url, FILE, headers, api_key, Year)
+    parser.parsing_file()
 
-# if __name__ == "__main__":
-#     url = 'https://www.sec.gov/Archives/edgar/data/40704/0001193125-23-177500.txt'
-#     headers = {"User-Agent": "bxie43@wisc.edu"}
-#     FILE = '10-K'
-    
-#     parser = DocScraper(url, headers, FILE)
-#     item = parser.parsing_file()
-#     print(item)
+    print("Item 1A:", parser.item1a)
+    print("Regulatory_compliance:", parser.regulatorycompliance)
+    print("----------------------")
+    # print("Item 1a:", parser.item1a)
+    # print("----------------------")
+    # print("Item 2:", parser.item2)
+    # print("----------------------")
+    # print("Item 7:", parser.item7)
+    # print("----------------------")
+    # print("Item 8:", parser.item8)    
